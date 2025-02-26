6801934933f2695cd3f8d6637b07a9beeca07b96
drrobot333 <drrobot333@naver.com>
2025-02-26 19:39:42 +0900

    feat: Change run.py

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 from Adam.ADAM import ADAM
 
-with open("API_key.txt", 'r') as key_file:
-    openai_api_key = key_file.read()
 
 ADAM = ADAM(
-    mc_port=52832,
-    llm_model_type='gpt-4-turbo',
-    use_local_llm_service=False,
-    openai_api_key=openai_api_key,
+    mc_port=25565,
+    llm_model_type='Qwen-v2.5-72B-AWQ',
+    use_local_llm_service=True,
+    openai_api_key="",
     auto_load_ckpt=True,
-    parallel=True
+    parallel=False,
 )
 
-ADAM.explore(['iron_ingot'], ['grass'])
+ADAM.explore(['wooden_pickaxe', 'wooden_sword', 'wooden_shovel', 'wooden_axe'], ['grass'])
+# ADAM.explore(['diamond'], ['grass'])

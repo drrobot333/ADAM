da75a41543934ab6b706775d2bddd4b84c214880
Suhwan Kim <30339175+drrobot333@users.noreply.github.com>
2025-03-02 15:15:20 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from Adam.ADAM import ADAM
 
+goal = [['wooden_pickaxe', 'wooden_sword', 'wooden_shovel', 'wooden_axe'], ['grass']]
 
 ADAM = ADAM(
     mc_port=25565,
@@ -8,7 +9,8 @@ ADAM = ADAM(
     openai_api_key="",
     auto_load_ckpt=True,
     parallel=False,
+    goal=goal
 )
 
-ADAM.explore(['wooden_pickaxe', 'wooden_sword', 'wooden_shovel', 'wooden_axe'], ['grass'])
+ADAM.explore(goal[0], goal[1])
 # ADAM.explore(['diamond'], ['grass'])

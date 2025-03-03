5e2c26a55a6dffc4e4b0641fefcd068a58eb1372
Suhwan Kim <30339175+drrobot333@users.noreply.github.com>
2025-03-03 13:07:51 +0900

    Update MLLM_API.py

--- Adam/MLLM_API.py
+++ Adam/MLLM_API.py
@@ -3,7 +3,7 @@ import requests
 
 def get_image_description(image_path='Adam/Adam/game_image/tmp.png', local_mllm_port=7000):
     text = 'Please describe this Minecraft image'
-    url = 'http://localhost:' + str(local_mllm_port) + '/send_image_text'
+    url = 'http://ksh_mllm_server:' + str(local_mllm_port) + '/send_image_text'
     files = {'image': open(image_path, 'rb')}
     data = {'text': text}
 
